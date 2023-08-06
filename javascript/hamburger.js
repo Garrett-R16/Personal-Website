6b385ad0eaf9e00e1995f323b6995a4b6e95107f
Garrett-R16 <16garrett.russell@gmail.com>
2023-08-05 20:29:44 -0400

    Fixed hamburger menu, minor color fixes, and fixed second project slideshow

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -1,31 +1,20 @@
 window.onload = function () {
   const menu_btn = document.querySelector('.hamburger');
   const mobile_menu = document.querySelector('.mobile-nav');
+  const scroll_limit = document.querySelector('body');
+  const mobile_nav = document.querySelector('.mobile-nav ul')
 
   menu_btn.addEventListener('click', function () {
       menu_btn.classList.toggle('is-active');
-      mobile_menu.classList.toggle('is-active');    
+      mobile_menu.classList.toggle('is-active');  
+      scroll_limit.classList.toggle('is-active');   
   });
 
-  var counter = 1;
-  setInterval(function(){
-    document.getElementById('image' + counter).checked = true;
-    counter++;
-    if(counter > 3){
-      counter = 1;
-    }
-  }, 5000);
-
-  function setWidth() {
-    var one = document.getElementById(".navigation-auto");
-    var two = document.getElementById(".navigation-manual");
-    style = window.getComputedStyle(one);
-    wdt = style.getPropertyValue('width');
-    two.style.width = wdt;
-  }
-  
+  mobile_nav.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');  
+    scroll_limit.classList.toggle('is-active');   
+  });
 
 }
-
-
 
