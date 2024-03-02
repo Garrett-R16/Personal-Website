1fce908c563a3c95f732f8e365305a4c1d74d101
Garrett-R16 <16garrett.russell@gmail.com>
2024-03-02 11:08:35 -0500

    fixed js file name

---
+++ javascript/menu.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+  const menu_btn = document.querySelector('.hamburger');
+  const mobile_menu = document.querySelector('.mobile-nav');
+  const scroll_limit = document.querySelector('body');
+  const mobile_nav = document.querySelector('.mobile-nav ul')
+
+  menu_btn.addEventListener('click', function () {
+      menu_btn.classList.toggle('is-active');
+      mobile_menu.classList.toggle('is-active');
+      scroll_limit.classList.toggle('is-active');
+  });
+
+  mobile_nav.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+    scroll_limit.classList.toggle('is-active');
+  });
+
+}
+
